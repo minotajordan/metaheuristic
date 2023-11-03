beab01a0fa8abcf1ce323fa729be7a13dc48dbb0
Jordan Minota <popjj@live.com>
2023-11-03 15:43:08 -0500

    Create C_03_2023.m

---
+++ C_03_2023.m
@@ -0,0 +1,13 @@
+## Class 3 nov - 2023
+
+## Propuestos para optimizacion continua -> binaria -> discreta -> etc....
+## De los genericos -> partimos para cada problema diferente 
+
+LINK: https://flowchart.fun/
+
+Algoritmos Emjambres
+  James Kennedy y Eberhaty Russell en 1995
+    Basado en comportamientos sociales/colectivo
+      Algoritmo
+        Se genera una particula con la inicialización de otra particula
+        
